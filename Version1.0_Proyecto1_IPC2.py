53d321139c115e051b12c72d8206333b96c592f6
Diego Barrios <dialejandrobarriosg@gmail.com>
2022-08-19 22:28:09 -0600

    probando 1

---
+++ Version1.0_Proyecto1_IPC2.py
@@ -0,0 +1,28 @@
+from Paciente import Paciente
+from Filas import Filas
+from Matriz import Matriz
+from Pacientes import Pacientes
+from ListaDobleFila import ListaDobleFila
+from ListaDobleColum import ListaDobleColum
+
+
+columnas = ListaDobleColum()
+columnas.insertar(1)
+columnas.insertar(2)
+columnas.insertar(3)
+
+filas = Filas(1)
+filas.lista_columnas.insertar(columnas)
+matriz = Matriz(1)
+matriz.lita_filas.insertar(filas)
+
+paciente1 = Paciente('DiegoAlejandro','25','16','8')
+paciente1.listaMatriz.agregar_principio(matriz)
+
+
+matrices = Pacientes()
+
+matrices.agregarAlPrincipio(paciente1)
+
+
+matrices.recorrer()
